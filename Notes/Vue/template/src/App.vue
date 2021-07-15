cee38007d350f8b1e0ec1290262120a1690e24bd
Alexey Mironov <amironov73@gmail.com>
2021-07-15 11:30:39 +0800

    Add Vue template

---
+++ Notes/Vue/template/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Calculator></Calculator>
+  </div>
+</template>
+
+<script>
+
+import Calculator from './components/calc'
+
+export default {
+  name: 'App',
+
+
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 60px;
+}
+</style>
---
+++ Notes/Vue/template/src/components/calc.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+
+    <div class="row">
+      <div class="col calc-title">
+        Простой калькулятор, складывающий два числа
+      </div>
+    </div>
+
+    <form>
+
+      <div class="form-group row mb-2">
+        <label for="first" class="col-sm-3 offset-sm-3 form-label">
+          Первое слагаемое:
+        </label>
+        <div class="col-sm-3">
+          <input id="first" type="text" class="form-control" v-model="first" />
+        </div>
+      </div>
+
+      <div class="form-group row mb-2">
+        <label for="first" class="col-sm-3 offset-sm-3 form-label">
+          Второе слагаемое:
+        </label>
+        <div class="col-sm-3">
+          <input id="second" type="text" class="form-control" v-model="second" />
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label for="first" class="col-sm-3 offset-sm-3 form-label">
+          Сумма:
+        </label>
+        <div class="col-sm-3">
+          <input readonly id="summa" type="text" class="form-control" v-model="summa" />
+        </div>
+      </div>
+
+    </form>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "calc",
+
+  data: function () {
+    return {
+      first: 123,
+      second: 456,
+    }
+  },
+
+  computed: {
+    summa: function () {
+      return parseFloat(this.first) + parseFloat(this.second)
+    }
+  },
+}
+</script>
+
+<style scoped>
+.calc-title {
+  font-weight: bold;
+  text-align: center;
+  padding-bottom: 5mm;
+}
+
+</style>
